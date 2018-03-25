b0c96cbfbeac847e895c5a7a4995a03248f9e83c
Ammar Khaku <ammar.khaku@gmail.com>
2018-03-24 18:53:25 -0700

    Add link to home page

--- src/newtab/footer.js
+++ src/newtab/footer.js
@@ -5,8 +5,17 @@ import './footer.less';
 export default function Footer() {
   return (
     <div className="Container-Footer">
+      <br/>
       <a
-        className="Text-Report"
+        className="Text-Link"
+        href="http://gettabulator.com"
+        target="_blank"
+      >
+        {'gettabulator.com'}
+      </a>
+      {'|'}
+      <a
+        className="Text-Link"
         href="https://github.com/akhaku/tabulator/issues/new"
         target="_blank"
       >
